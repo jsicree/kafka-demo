6588473bf0da6a1a4f4abd7cd215eeb148600cf2
Joe Sicree <jsicree@gmail.com>
2019-03-16 11:36:45 -0400

    Removed old dataflow projects

--- dataflow-producer-client/src/main/java/com/kafkademo/dataflow/producer/DataflowProducerClient.java
+++
@@ -1,9 +0,0 @@
-package com.kafkademo.dataflow.producer;
-
-public interface DataflowProducerClient<T> {
-	
-	public void hello() throws DataflowProducerClientException;
-	
-	public void send(T value) throws DataflowProducerClientException;
-
-}
--- dataflow-test-client/src/main/java/com/kafkademo/dataflow/testclient/service/DispatcherService.java
+++
@@ -1,7 +0,0 @@
-package com.kafkademo.dataflow.testclient.service;
-
-public interface DispatcherService {
-
-	public void processMessage(String guid, String source, String body);
-	
-}
